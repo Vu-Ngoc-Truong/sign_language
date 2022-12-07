da0bece54f02fd85331c071340f180c284041acc
Vu-Ngoc-Truong <truong.vungoc@meiko.vn>
2022-12-07 22:18:37 +0700

    add audio

--- text_to_speech.py
+++ text_to_speech.py
@@ -9,10 +9,15 @@ from gtts import gTTS
 # import wikipedia
 # wikipedia.set_lang('vi')
 
-def text_to_speech(text, language='vi'):
-    output = gTTS(text,lang=language, slow=False)
-    output.save("output.mp3")
-    playsound.playsound('output.mp3', True)
-    os.remove("output.mp3")
 
-text_to_speech("Xin chào buổi tối")
+def text_to_speech(text, language='vi', save_sound=False):
+    output = gTTS(text,lang=language, slow=False)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    output.save(dir_path + "/output.mp3")
+    playsound.playsound('output.mp3', True)
+    if not save_sound:
+        os.remove(dir_path + "/output.mp3")
+
+if __name__ == '__main__':
+    # text_to_speech("Xin chào!")
+    text_to_speech("Xin chào!", save_sound=True)
